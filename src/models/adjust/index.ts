3c76a1eb4751dcf310f1d129967a4fb0fd507253
Tyler Long <tyler4long@gmail.com>
2023-03-24 11:42:48 -0700

    mergeOf

--- src/models/adjust/index.ts
+++ src/models/adjust/index.ts
@@ -2,12 +2,15 @@ import { NamedSchema } from '../../types';
 import { deRef } from './de-ref';
 import { fixFax } from './fax';
 import { fixGreeting } from './greeting';
+import { mergeOf } from './merge-of';
 import { ref } from './ref';
 
 export const adjust = (_schemas: NamedSchema[]): NamedSchema[] => {
-  let schemas = deRef(_schemas);
+  let schemas = mergeOf(_schemas);
+  schemas = deRef(schemas);
   schemas = ref(schemas);
   schemas = fixFax(schemas);
   schemas = fixGreeting(schemas);
+  // todo: fix miscellaneous issues
   return schemas;
 };
